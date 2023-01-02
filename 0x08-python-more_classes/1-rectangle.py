395249c8190af3446168a551afddefee94e45c44
Funmi2022 <Oluwafunmiketosin@gmail.com>
2023-01-02 15:11:18 -0800

    0x08-python-more_classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -43,4 +43,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = 
+        self.__height = value
