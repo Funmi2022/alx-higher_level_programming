7c4bb4a012f7b2e507952608df9731104b421c93
Funmi2022 <oluwafunmiketosin@gmail.com>
2023-03-31 07:32:55 -0700

    fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,4 @@
-sr/bin/python3
+#!/usr/bin/python3
 """Uses the GitHub API to display a GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
